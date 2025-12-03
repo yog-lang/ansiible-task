4aa4ab562368caa8c6881c654b65200e9dd59f9c
yog-lang <76519587+yog-lang@users.noreply.github.com>
2025-12-04 03:25:25 +0530

    Update create_tmp_file.py

--- library/create_tmp_file.py
+++ library/create_tmp_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
+## AnsibleModule is a fixed, built-in class provided by Ansible for writing custom modules.
 from ansible.module_utils.basic import AnsibleModule
 
+##creating a variable named module
+##This variable will store an object (an instance) created from the class AnsibleModule.
 def run_module():
     module = AnsibleModule(
         argument_spec=dict(
@@ -9,12 +12,15 @@ def run_module():
             content=dict(type='str', required=True)
         )
     )
-
+    
     file_path = f"/tmp/{module.params['name']}"
 
+## using open() function to open a file 
+## with : to automatically closes the file when done
+
     try:
-        with open(file_path, "w") as f:
-            f.write(module.params['content'])
+        with open(file_path, "w") as file_handle:
+            file_handle.write(module.params['content'])
 
         module.exit_json(
             changed=True,
